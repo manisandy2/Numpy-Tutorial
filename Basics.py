8c7bcf3efc30624e6cae0c8cf3d2d8e1f86096ff
unknown <mani.sandy2@gmail.com>
2022-12-06 17:03:10 +0530

    numpy basic file

---
+++ Basics.py
@@ -0,0 +1,70 @@
+import numpy as np
+# 1.An example
+# a = np.arange(15).reshape(3,5)
+
+# print(a.shape)
+
+# print(a.ndim)
+
+# print(a.dtype.name)
+
+# print(type(a))
+
+# b = np.array([6,7,8])
+
+# print(b)
+
+# print(type(b))
+
+# 2.Array Creation
+
+# a = np.array([1,3,5,4])
+
+# print(a.dtype)
+
+# b = np.array([0.5,0.6,0.8])
+# print(b.dtype)
+
+# 2.1 two-dimensional arrays
+
+# a = np.array([(1,5,7),(9,6,3)],dtype=float)
+# print(a)
+
+# 2.2 zeros and ones
+
+# a = np.zeros((3,4))
+# print(a)
+
+# b = np.ones((3,3,4))
+# print(b)
+
+# 2.3  arange int & float
+
+# a = np.arange(0,100,5)
+# print(a)
+
+# b = np.arange(0,2,0.3)
+# print(b)
+
+# 3.Printing Arrays
+
+
+# 3.1  1d array
+
+# a = np.arange(10)
+# print("1d array")
+# print("")
+# print(a)
+# # 3.2  2d array
+
+# b = np.arange(10).reshape(2,5)
+# print("2nd array")
+# print("")
+# print(b)
+
+# # 3.3 3d array
+
+# c = np.arange(24).reshape(3,2,4)
+# print("3d array")
+# print("")
+# print(c)
